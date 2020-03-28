8d0508c13403514ddb062acd71fbe08fa9338606
checksonhk <jackson.to@gmail.com>
2020-03-28 15:09:07 -0400

    add type checking to header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./layouts/Hero/Hero";
+import Header from "./components/Header.jsx/Header";
+
+function App() {
+  return (
+    <div id="app">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
