1f2448f7cfcc621b3a37239df2181b2d1c3a8d7d
Frank Lam <frank.is.missing@gmail.com>
2020-03-30 17:41:08 -0700

    Added z-index and absolute positioning to parent element

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import ProfileSummary from "./layouts/ProfileSummary";
 
 function App() {
   return (
-    <ProfileSummary />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit
-    //       <code>src/App.js</code>
-    //       and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
