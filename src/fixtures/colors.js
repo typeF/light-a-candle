7778c6bc6169980cad926f94227d72eeb8a7efca
Benard Kong <benardkong@hotmail.com>
2020-03-30 21:31:29 -0700

    Submit profile shows up when user clicks on 'Add a hero' button in footer

---
+++ src/fixtures/colors.js
@@ -0,0 +1,13 @@
+export const darkGreen = "#1e2a32";
+
+export const sageGreen = "#636c76";
+
+export const mistGreen = "#a5b9bd";
+
+export const darkPurple = "#1c161f";
+
+export const lilac = "#d9cdf0";
+
+export const candleLight = "#fcef79";
+
+export const white = "#fff";
--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import CandleWrapper from "../CandleWrapper/CandleWrapper";
 import COVIDBanner from "../../components/COVIDBanner/COVIDBanner";
+import AddHeroButton from "../../components/AddHeroButton/AddHeroButton";
+import SubmitProfile from "../SubmitProfile";
 
 const FooterContainer = styled.footer`
   width: 100%;
@@ -10,10 +12,32 @@ const FooterContainer = styled.footer`
   pointer-events: initial;
 `;
 
+const IconsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
 function Footer({ isMainPage, handleNotification }) {
+  const [onlyShowIcon, setOnlyShowIcon] = useState("");
+
   return (
     <FooterContainer>
-      {isMainPage ? <COVIDBanner /> : <CandleWrapper addNotification={handleNotification} />}
+      {isMainPage ? (
+        <COVIDBanner />
+      ) : (
+        <IconsContainer>
+          {onlyShowIcon !== "addHero" && onlyShowIcon !== "lightCandle" && (
+            <AddHeroButton />
+          ) /* This is just a filler until it gets hooked up as well; */}
+          {onlyShowIcon !== "candlesLit" && onlyShowIcon !== "addHero" && (
+            <CandleWrapper addNotification={handleNotification} setOnlyShowIcon={setOnlyShowIcon} />
+          )}
+          {onlyShowIcon !== "candlesLit" && onlyShowIcon !== "lightCandle" && (
+            <AddHeroButton handleClick={setOnlyShowIcon} />
+          )}
+        </IconsContainer>
+      )}
+      {onlyShowIcon === "addHero" && <SubmitProfile handleClose={() => setOnlyShowIcon("")} />}
     </FooterContainer>
   );
 }
