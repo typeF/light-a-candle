5520b5b2fe0e330ebb778b05d9903d852d8f8058
Frank Lam <frank.is.missing@gmail.com>
2020-04-01 17:40:43 -0700

    A

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,3 +1,4 @@
 // TODO: Replace first server with production server
-const server = process.env.PRODUCTION ? "http://localhost:4000" : "http://localhost:4000";
+// const server = process.env.PRODUCTION ? "http://localhost:4000" : "http://localhost:4000";
+const server = "https://light-a-candle.herokuapp.com";
 export default server;
