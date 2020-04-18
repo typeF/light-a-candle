6cf21c2574976a400cb215ab4af5b23985056056
Frank Lam <frank.is.missing@gmail.com>
2020-04-18 14:35:32 -0700

    Memoized addMarkers function so satisfy dependecy warnings from react

--- client/src/components/InputComponent/InputComponent.jsx
+++ client/src/components/InputComponent/InputComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import colors from "../../fixtures/colors";
-import { fontFamily } from "../../fixtures/typography";
+// import { fontFamily } from "../../fixtures/typography";
 
 const Container = styled.div(({ customStyles }) => ({
   "border-bottom": `1px solid ${colors.lilac}`,
--- client/src/layouts/Mapbox/Mapbox.js
+++ client/src/layouts/Mapbox/Mapbox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef, useMemo } from "react";
 import styled from "styled-components";
 import "mapbox-gl/dist/mapbox-gl.css";
 import PropTypes from "prop-types";
@@ -11,27 +11,32 @@ const MapContainer = styled.div`
 `;
 
 const Mapbox = ({ handleDrawer, setLocation, setMemorials, geoJsonData, setGeoJsonData }) => {
-  const [map, setMap] = useState(null);
+  const map = useRef(null);
   const mapContainer = useRef(null);
+  const setMap = (mapInstance) => {
+    map.current = mapInstance;
+  };
 
-  const addMarkers = createMarkers({ handleDrawer, setLocation, setMemorials });
+  const addMarkers = useMemo(() => {
+    return createMarkers({ handleDrawer, setLocation, setMemorials });
+  }, [handleDrawer, setLocation, setMemorials]);
 
   useEffect(() => {
-    if (!map) initMap({ setMap, mapContainer, setGeoJsonData, addMarkers });
-  }, [map]);
+    if (!map.current) initMap({ setMap, mapContainer, setGeoJsonData, addMarkers });
+  }, [addMarkers, setGeoJsonData]);
 
   useEffect(() => {
-    if (map) {
+    if (map.current) {
       document.querySelectorAll(".mapboxgl-marker").forEach((marker) => marker.remove());
-      // Needs a setTimeout to render markers properly otherwise mapbox will use
-      // their default marker styling, not sure why
+      // Needs a setTimeout to render markers properly otherwise
+      // mapbox will use their default marker styling, not sure why
       setTimeout(() => {
-        addMarkers(map)(geoJsonData);
+        addMarkers(map.current)(geoJsonData);
         // Refreshes map
-        map.getSource("points").setData(geoJsonData);
+        map.current.getSource("points").setData(geoJsonData);
       }, 0);
     }
-  }, [geoJsonData]);
+  }, [geoJsonData, addMarkers, setGeoJsonData]);
 
   /* eslint-disable no-return-assign */
   return (
